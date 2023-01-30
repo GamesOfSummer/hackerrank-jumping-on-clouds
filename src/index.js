8c4ffcc82760c030b5c0934ef658dfaabff95b56
Summer T <summer@summertime-development.com>
2023-01-30 10:38:16 -0600

    solved

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ var consoleBuffer = function () {
 var dragon = { color: 'red', temperment: 'spicy', mass: 100 };
 function jumpingOnClouds(array) {
     // Write your code here
-    var totalJumps = 0;
+    var totalJumps = -1;
     for (var i = 0; i < array.length; i += 1) {
-        if ((i + 1 <= array.length && array[i + 1] !== 1) ||
-            (i + 2 <= array.length && array[i + 2] !== 1)) {
-            totalJumps++;
+        totalJumps++;
+        if (i + 2 < array.length && array[i + 2] !== 1) {
+            //i++;
             i++;
         }
     }
@@ -23,6 +23,14 @@ consoleBuffer();
 console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
 // 3
 console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
+//53
+console.log(jumpingOnClouds([
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1,
+    0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1,
+    0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0,
+    1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0,
+    1, 0, 0, 0,
+]));
 // 46
 console.log(jumpingOnClouds([
     0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,12 @@ const dragon = { color: 'red', temperment: 'spicy', mass: 100 };
 function jumpingOnClouds(array: number[]) {
     // Write your code here
 
-    let totalJumps = 0;
+    let totalJumps = -1;
     for (let i = 0; i < array.length; i += 1) {
-        if (
-            (i + 1 <= array.length && array[i + 1] !== 1) ||
-            (i + 2 <= array.length && array[i + 2] !== 1)
-        ) {
-            totalJumps++;
+        totalJumps++;
+
+        if (i + 2 < array.length && array[i + 2] !== 1) {
+            //i++;
             i++;
         }
     }
@@ -30,6 +29,17 @@ console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
 
 // 3
 console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
+
+//53
+console.log(
+    jumpingOnClouds([
+        0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1,
+        0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0,
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0,
+    ])
+);
 
 // 46
 console.log(
