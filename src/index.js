edee976801bcfb3b8dae7cc8d9195e721fb5052b
Summer T <summer@summertime-development.com>
2023-01-30 09:26:47 -0600

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var consoleBuffer = function () {
+    console.log('--------------------------');
+    console.log('--------------------------');
+    console.log('\n');
+};
+var dragon = { color: 'red', temperment: 'spicy', mass: 100 };
+consoleBuffer();
+console.log();
+consoleBuffer();
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const consoleBuffer = () => {
+    console.log('--------------------------');
+    console.log('--------------------------');
+    console.log('\n');
+};
+
+const dragon = { color: 'red', temperment: 'spicy', mass: 100 };
+
+consoleBuffer();
+
+console.log();
+
+consoleBuffer();
+
+export {};
