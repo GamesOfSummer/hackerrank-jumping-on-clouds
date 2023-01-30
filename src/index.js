55daf9a46a48a90ee37c782440cf240ec4b02cf6
Summer T <summer@summertime-development.com>
2023-01-30 09:59:52 -0600

    progress

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ var consoleBuffer = function () {
     console.log('\n');
 };
 var dragon = { color: 'red', temperment: 'spicy', mass: 100 };
+function jumpingOnClouds(array) {
+    // Write your code here
+    var totalJumps = 0;
+    for (var i = 0; i < array.length; i += 1) {
+        if (array[i] === 0) {
+            totalJumps++;
+        }
+        else {
+            if (i + 1 <= array.length && array[i + 1] === 0) {
+                //totalJumps++;
+                i++;
+            }
+        }
+    }
+    return totalJumps;
+}
 consoleBuffer();
-console.log();
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
+console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
 consoleBuffer();
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,28 @@ const consoleBuffer = () => {
 
 const dragon = { color: 'red', temperment: 'spicy', mass: 100 };
 
+function jumpingOnClouds(array: number[]) {
+    // Write your code here
+
+    let totalJumps = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] === 0) {
+            totalJumps++;
+        } else {
+            if (i + 1 <= array.length && array[i + 1] === 0) {
+                //totalJumps++;
+                i++;
+            }
+        }
+    }
+
+    return totalJumps;
+}
+
 consoleBuffer();
 
-console.log();
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
+console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
 
 consoleBuffer();
 
